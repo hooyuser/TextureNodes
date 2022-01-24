f9442a1a247b955d647936dcd691a2b3a5851bd5
Derived Cat <hooyuser@outlook.com>
2022-01-24 23:40:54 +0800

    Update gui_node_editor.cpp

--- source/gui/gui_node_editor.cpp
+++ source/gui/gui_node_editor.cpp
@@ -1,7 +1,6 @@
 #include "gui_node_editor.h"
 #include "../vk_engine.h"
 #include "imgui_internal.h"
-#include "imgui.h"
 
 namespace node_ed {
 	static ed::EditorContext* context = nullptr;
@@ -28,9 +27,9 @@ void create_nodes() {
 static Node* create_node_add() {
 	node_ed::nodes.emplace_back(get_next_id(), "Add", NodeType::ADD);
 	auto& node = node_ed::nodes.back();
-	node.inputs.emplace_back(get_next_id(), "value 1", PinType::FLOAT);
-	node.inputs.emplace_back(get_next_id(), "value 2", PinType::FLOAT);
-	node.outputs.emplace_back(get_next_id(), "result", PinType::FLOAT);
+	node.inputs.emplace_back(get_next_id(), "Value", PinType::FLOAT);
+	node.inputs.emplace_back(get_next_id(), "Value", PinType::FLOAT);
+	node.outputs.emplace_back(get_next_id(), "Result", PinType::FLOAT);
 
 	build_node(&node);
 	//ed::SetNodePosition(node.id, ImVec2(500, 70));
@@ -51,27 +50,59 @@ static void build_node(Node* node) {
 }
 
 void draw_node_editer() {
+	ImGui::ShowDemoWindow();
 	ed::SetCurrentEditor(node_ed::context);
 	ed::Begin("My Editor", ImVec2(0.0f, 0.0f));
 	// Start drawing nodes.
 	for (auto& node : node_ed::nodes) {
 		ed::BeginNode(node.id);
-		
 		ImGui::TextUnformatted(node.name.c_str());
-		//auto rect = imgui_get_item_rect();
-		//ImGui::GetWindowDrawList()->AddRectFilled(rect.GetTL(), rect.GetBR(), ImGui::ColorConvertFloat4ToU32(ImVec4(1, .15, .15, 1)));
+		ImGui::Dummy(ImVec2(140.0f, 3.0f));
+		auto node_rect = imgui_get_item_rect();
+		ImGui::GetWindowDrawList()->AddRectFilled(node_rect.GetTL(), node_rect.GetBR(), ImColor(68, 129, 196, 160));
 		//ImGui::BeginVertical("delegates", ImVec2(0, 28));
-		for (auto& pin : node.outputs) {
-			ed::BeginPin(get_next_id(), ed::PinKind::Output);
-			ImGui::Text(pin.name.c_str());
-			ed::EndPin();
+
+		if (ImGui::BeginTable("table1", 1)) {
+			for (auto& pin : node.outputs) {
+				ImGui::TableNextColumn();
+				ed::PushStyleVar(ed::StyleVar_PinCorners, 15);
+				ed::BeginPin(get_next_id(), ed::PinKind::Output);
+				auto rect = imgui_get_item_rect();
+				ed::PinPivotRect(rect.GetCenter(), rect.GetCenter());
+				ed::PinRect(rect.GetTL(), rect.GetBR());
+
+				auto str = pin.name.c_str();
+				ImGui::SetCursorPosX(node_rect.Max.x - ImGui::CalcTextSize(str).x);
+
+				ImGui::Text(str);
+				rect = imgui_get_item_rect();
+
+				auto drawList = ImGui::GetWindowDrawList();
+				drawList->AddCircleFilled(ImVec2(node_rect.Max.x + 8.0f, rect.GetCenter().y), 5.8f, ImColor(68, 129, 196, 160), 24);
+				drawList->AddCircle(ImVec2(node_rect.Max.x + 8.0f, rect.GetCenter().y), 5.8f, ImColor(68, 129, 196, 255), 24, 1.8);
+				ed::EndPin();
+				ed::PopStyleVar(1);
+			}
+
+			for (auto& pin : node.inputs) {
+				ImGui::TableNextColumn();
+				ed::PushStyleVar(ed::StyleVar_PinCorners, 15);
+				ed::BeginPin(get_next_id(), ed::PinKind::Input);
+				auto rect = imgui_get_item_rect();
+				ed::PinPivotRect(rect.GetCenter(), rect.GetCenter());
+				ed::PinRect(rect.GetTL(), rect.GetBR());
+				ImGui::Text(pin.name.c_str());
+				rect = imgui_get_item_rect();
+
+				auto drawList = ImGui::GetWindowDrawList();
+				drawList->AddCircleFilled(ImVec2(rect.Min.x - 8.0f, rect.GetCenter().y), 5.8f, ImColor(68, 129, 196, 160), 24);
+				drawList->AddCircle(ImVec2(rect.Min.x - 8.0f, rect.GetCenter().y), 5.8f, ImColor(68, 129, 196, 255), 24, 1.8);
+				ed::EndPin();
+				ed::PopStyleVar(1);
+			}
+			ImGui::EndTable();
 		}
 		
-		for (auto& pin : node.inputs) {
-			ed::BeginPin(get_next_id(), ed::PinKind::Input);
-			ImGui::Text(pin.name.c_str());
-			ed::EndPin();
-		}
 		//ImGui::SameLine();
 		ed::EndNode();
 		
