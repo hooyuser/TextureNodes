d709a74c92f54d5ac00d7df47c530bd87b5b3a6d
Derived Cat <hooyuser@outlook.com>
2022-03-02 00:46:32 +0800

    update ImageData

--- source/gui/gui_node_data.h
+++ source/gui/gui_node_data.h
@@ -97,7 +97,7 @@ struct ImageData : public NodeData {
 	uint32_t width = 1024;
 	uint32_t height = 1024;
 
-	inline static VkDescriptorSetLayout descriptor_set_layout = nullptr;
+	inline static VkDescriptorSetLayout ubo_descriptor_set_layout = nullptr;
 	inline static VkPipelineLayout image_pocessing_pipeline_layout = nullptr;
 	inline static VkPipeline image_pocessing_pipeline = nullptr;
 	inline static VkRenderPass image_pocessing_render_pass = nullptr;
@@ -115,11 +115,51 @@ struct ImageData : public NodeData {
 	//}
 	//ImageData(){};
 	ImageData(VulkanEngine* engine) :engine(engine) {
+		create_fence();
+
+		create_texture();
+
+		uniform_buffer = engine::Buffer::createBuffer(engine,
+			sizeof(UboType),
+			VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			SWAPCHAIN_INDEPENDENT_BIT);
+
+		if (ubo_descriptor_set_layout == nullptr) {
+			create_ubo_descriptor_set_layout();
+		}
+
+		create_ubo_descriptor_set();
+
+		update_descriptor_sets();
+
+		if (image_pocessing_render_pass == nullptr) {
+			create_image_pocessing_render_pass();
+			create_image_pocessing_pipeline_layout();
+			create_image_pocessing_pipeline();
+		}
+
+		create_framebuffer();
+
+		create_command_buffer();
+	}
+
+	~ImageData() {
+		engine->node_texture_manager.delete_id(node_texture_id);
+		vkFreeCommandBuffers(engine->device, engine->commandPool, 1, &node_cmd_buffer);
+		vkDestroyFramebuffer(engine->device, image_pocessing_framebuffer, nullptr);
+		vkFreeDescriptorSets(engine->device, engine->node_texture_manager.descriptor_pool, 1, &ubo_descriptor_set);
+		vkDestroyFence(engine->device, fence, nullptr);
+	}
+
+	void create_fence() {
 		auto fenceInfo = vkinit::fenceCreateInfo(VK_FENCE_CREATE_SIGNALED_BIT);
 		if (vkCreateFence(engine->device, &fenceInfo, nullptr, &fence) != VK_SUCCESS) {
 			throw std::runtime_error("failed to create fence!");
 		}
+	}
 
+	void create_texture() {
 		texture = engine::Texture::create_2D_render_target(engine,
 			1024,
 			1024,
@@ -131,30 +171,28 @@ struct ImageData : public NodeData {
 		gui_texture = ImGui_ImplVulkan_AddTexture(texture->sampler, texture->imageView, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
 		node_texture_id = engine->node_texture_manager.get_id();
+	}
 
-		uniform_buffer = engine::Buffer::createBuffer(engine,
-			sizeof(UboType),
-			VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
-			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-			SWAPCHAIN_INDEPENDENT_BIT);
+	void create_ubo_descriptor_set_layout() {
+		auto ubo_layout_binding = vkinit::descriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_FRAGMENT_BIT, 0);
+		std::array layout_bindings = { ubo_layout_binding };
+		engine->create_descriptor_set_layout(layout_bindings, ubo_descriptor_set_layout);
+	}
 
-		if (descriptor_set_layout == nullptr) {
-			auto ubo_layout_binding = vkinit::descriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_FRAGMENT_BIT, 0);
-			std::array layout_bindings = { ubo_layout_binding };
-			engine->create_descriptor_set_layout(layout_bindings, descriptor_set_layout);
-		}
-
-		VkDescriptorSetAllocateInfo descriptor_alloc_info{
+	void create_ubo_descriptor_set() {
+		VkDescriptorSetAllocateInfo ubo_descriptor_alloc_info{
 			.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO,
 			.descriptorPool = engine->node_texture_manager.descriptor_pool,
 			.descriptorSetCount = 1,
-			.pSetLayouts = &descriptor_set_layout,
+			.pSetLayouts = &ubo_descriptor_set_layout,
 		};
 
-		if (vkAllocateDescriptorSets(engine->device, &descriptor_alloc_info, &ubo_descriptor_set) != VK_SUCCESS) {
+		if (vkAllocateDescriptorSets(engine->device, &ubo_descriptor_alloc_info, &ubo_descriptor_set) != VK_SUCCESS) {
 			throw std::runtime_error("failed to allocate descriptor sets!");
 		}
+	}
 
+	void update_descriptor_sets() {
 		VkDescriptorBufferInfo uniform_buffer_info{
 			.buffer = uniform_buffer->buffer,
 			.offset = 0,
@@ -190,9 +228,10 @@ struct ImageData : public NodeData {
 
 		vkUpdateDescriptorSets(engine->device, descriptor_writes.size(), descriptor_writes.data(), 0, nullptr);
 
-		//create renderpass
-		if (image_pocessing_render_pass == nullptr) {
-			VkAttachmentDescription colorAttachment{
+	}
+
+	void create_image_pocessing_render_pass() {
+		VkAttachmentDescription colorAttachment{
 				.format = VK_FORMAT_R8G8B8A8_UNORM,
 				.samples = VK_SAMPLE_COUNT_1_BIT,
 				.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
@@ -201,109 +240,107 @@ struct ImageData : public NodeData {
 				.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
 				.initialLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
 				.finalLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
-			};
+		};
 
-			VkAttachmentReference colorAttachmentRef{
-				.attachment = 0,
-				.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
-			};
+		VkAttachmentReference colorAttachmentRef{
+			.attachment = 0,
+			.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+		};
 
-			VkSubpassDescription subpass{
-				.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS,
-				.colorAttachmentCount = 1,
-				.pColorAttachments = &colorAttachmentRef,
-			};
+		VkSubpassDescription subpass{
+			.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS,
+			.colorAttachmentCount = 1,
+			.pColorAttachments = &colorAttachmentRef,
+		};
 
-			std::array dependencies{
-				VkSubpassDependency {
-					.srcSubpass = VK_SUBPASS_EXTERNAL,
-					.dstSubpass = 0,
-					.srcStageMask = VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
-					.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT |
-									VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT,
-					.srcAccessMask = 0,
-					.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT |
-									 VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
-					.dependencyFlags = 0,
-				},
-				VkSubpassDependency {
-					.srcSubpass = 0,
-					.dstSubpass = VK_SUBPASS_EXTERNAL,
-					.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
-					.dstStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
-					.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
-					.dstAccessMask = VK_ACCESS_UNIFORM_READ_BIT,
-					.dependencyFlags = 0,
-				}
-			};
+		std::array dependencies{
+			VkSubpassDependency {
+				.srcSubpass = VK_SUBPASS_EXTERNAL,
+				.dstSubpass = 0,
+				.srcStageMask = VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
+				.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT |
+								VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT,
+				.srcAccessMask = 0,
+				.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT |
+								 VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+				.dependencyFlags = 0,
+			},
+			VkSubpassDependency {
+				.srcSubpass = 0,
+				.dstSubpass = VK_SUBPASS_EXTERNAL,
+				.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+				.dstStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
+				.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+				.dstAccessMask = VK_ACCESS_UNIFORM_READ_BIT,
+				.dependencyFlags = 0,
+			}
+		};
 
-			std::array attachments = { colorAttachment };
+		std::array attachments = { colorAttachment };
 
-			VkRenderPassCreateInfo render_pass_info{
-				.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
+		VkRenderPassCreateInfo render_pass_info{
+			.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
+			.pNext = nullptr,
+			.attachmentCount = static_cast<uint32_t>(attachments.size()),
+			.pAttachments = attachments.data(),
+			.subpassCount = 1,
+			.pSubpasses = &subpass,
+			.dependencyCount = static_cast<uint32_t>(dependencies.size()),
+			.pDependencies = dependencies.data(),
+		};
+
+		if (vkCreateRenderPass(engine->device, &render_pass_info, nullptr, &image_pocessing_render_pass) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create render pass!");
+		}
+
+		engine->main_deletion_queue.push_function([=]() {
+			vkDestroyRenderPass(engine->device, image_pocessing_render_pass, nullptr);
+			});
+	}
+
+	void create_image_pocessing_pipeline_layout() {
+		auto descriptor_set_layouts = constexpr_if<has_field_type_v<UboType, TextureIdData>>(
+			std::array{ ubo_descriptor_set_layout, engine->node_texture_manager.descriptor_set_layout },
+			std::array{ ubo_descriptor_set_layout }
+		);
+
+		VkPipelineLayoutCreateInfo image_pocessing_pipeline_info = vkinit::pipelineLayoutCreateInfo(descriptor_set_layouts);
+
+		if (vkCreatePipelineLayout(engine->device, &image_pocessing_pipeline_info, nullptr, &image_pocessing_pipeline_layout) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create pipeline layout!");
+		}
+
+		engine->main_deletion_queue.push_function([=]() {
+			vkDestroyPipelineLayout(engine->device, image_pocessing_pipeline_layout, nullptr);
+			});
+	}
+
+	void create_image_pocessing_pipeline() {
+		engine::PipelineBuilder pipeline_builder(engine, engine::ENABLE_DYNAMIC_VIEWPORT, engine::DISABLE_VERTEX_INPUT);
+		std::array<std::string, sizeof...(Shaders)> shader_files;;
+		size_t i = 0;
+		((shader_files[i++] = Shaders.value), ...);
+		auto shaders = engine::Shader::createFromSpv(engine, shader_files);
+
+		for (auto shader_module : shaders->shaderModules) {
+			VkPipelineShaderStageCreateInfo shader_info{
+				.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
 				.pNext = nullptr,
-				.attachmentCount = static_cast<uint32_t>(attachments.size()),
-				.pAttachments = attachments.data(),
-				.subpassCount = 1,
-				.pSubpasses = &subpass,
-				.dependencyCount = static_cast<uint32_t>(dependencies.size()),
-				.pDependencies = dependencies.data(),
+				.stage = shader_module.stage,
+				.module = shader_module.shader,
+				.pName = "main"
 			};
-
-			if (vkCreateRenderPass(engine->device, &render_pass_info, nullptr, &image_pocessing_render_pass) != VK_SUCCESS) {
-				throw std::runtime_error("failed to create render pass!");
-			}
-
-			engine->main_deletion_queue.push_function([=]() {
-				vkDestroyRenderPass(engine->device, image_pocessing_render_pass, nullptr);
-				});
+			pipeline_builder.shaderStages.emplace_back(std::move(shader_info));
 		}
 
-		if (image_pocessing_pipeline_layout == nullptr) {
+		pipeline_builder.buildPipeline(engine->device, image_pocessing_render_pass, image_pocessing_pipeline_layout, image_pocessing_pipeline);
 
-			auto descriptor_set_layouts = constexpr_if<has_field_type_v<UboType, TextureIdData>>(
-				std::array{ descriptor_set_layout, engine->node_texture_manager.descriptor_set_layout },
-				std::array{ descriptor_set_layout }
-			);
+		engine->main_deletion_queue.push_function([=]() {
+			vkDestroyPipeline(engine->device, image_pocessing_pipeline, nullptr);
+			});
+	}
 
-			VkPipelineLayoutCreateInfo image_pocessing_pipeline_info = vkinit::pipelineLayoutCreateInfo(descriptor_set_layouts);
-
-			if (vkCreatePipelineLayout(engine->device, &image_pocessing_pipeline_info, nullptr, &image_pocessing_pipeline_layout) != VK_SUCCESS) {
-				throw std::runtime_error("failed to create pipeline layout!");
-			}
-
-			engine->main_deletion_queue.push_function([=]() {
-				vkDestroyPipelineLayout(engine->device, image_pocessing_pipeline_layout, nullptr);
-				});
-		}
-
-		if (image_pocessing_pipeline == nullptr) {
-			engine::PipelineBuilder pipeline_builder(engine, engine::ENABLE_DYNAMIC_VIEWPORT, engine::DISABLE_VERTEX_INPUT);
-			std::array<std::string, sizeof...(Shaders)> shader_files;;
-			size_t i = 0;
-			((shader_files[i++] = Shaders.value), ...);
-			auto shaders = engine::Shader::createFromSpv(engine, shader_files);
-
-			for (auto shader_module : shaders->shaderModules) {
-				VkPipelineShaderStageCreateInfo shader_info{
-					.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
-					.pNext = nullptr,
-					.stage = shader_module.stage,
-					.module = shader_module.shader,
-					.pName = "main"
-				};
-				pipeline_builder.shaderStages.emplace_back(std::move(shader_info));
-			}
-
-			pipeline_builder.buildPipeline(engine->device, image_pocessing_render_pass, image_pocessing_pipeline_layout, image_pocessing_pipeline);
-
-			engine->main_deletion_queue.push_function([=]() {
-				vkDestroyPipeline(engine->device, image_pocessing_pipeline, nullptr);
-				});
-		}
-
-		//create framebuffer
-
+	void create_framebuffer() {
 		std::array framebuffer_attachments = { texture->imageView };
 
 		VkFramebufferCreateInfo framebufferInfo = vkinit::framebufferCreateInfo(image_pocessing_render_pass, VkExtent2D{ width , height }, framebuffer_attachments);
@@ -311,9 +348,9 @@ struct ImageData : public NodeData {
 		if (vkCreateFramebuffer(engine->device, &framebufferInfo, nullptr, &image_pocessing_framebuffer) != VK_SUCCESS) {
 			throw std::runtime_error("failed to create framebuffer!");
 		}
+	}
 
-		//create command buffer
-
+	void create_command_buffer() {
 		VkCommandBufferAllocateInfo cmdAllocInfo = vkinit::commandBufferAllocateInfo(engine->commandPool, 1);
 
 		if (vkAllocateCommandBuffers(engine->device, &cmdAllocInfo, &node_cmd_buffer) != VK_SUCCESS) {
@@ -365,15 +402,6 @@ struct ImageData : public NodeData {
 		}
 	}
 
-	~ImageData() {
-		engine->node_texture_manager.delete_id(node_texture_id);
-		vkFreeCommandBuffers(engine->device, engine->commandPool, 1, &node_cmd_buffer);
-		vkDestroyFramebuffer(engine->device, image_pocessing_framebuffer, nullptr);
-		vkFreeDescriptorSets(engine->device, engine->node_texture_manager.descriptor_pool, 1, &ubo_descriptor_set);
-		vkDestroyFence(engine->device, fence, nullptr);
-	}
-
-	//template<typename T>
 	void update_ubo(const PinVariant& value, size_t index) {
 		UboType::Class::FieldAt(index, [&](auto& field) {
 			using PinT = std::decay_t<decltype(field)>::Type;
@@ -387,6 +415,3 @@ struct ImageData : public NodeData {
 
 template<typename UboType, StringLiteral ...Shaders>
 using ImageDataPtr = std::shared_ptr<ImageData<UboType, Shaders...>>;
-
-
-
